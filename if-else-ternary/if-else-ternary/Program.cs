4ff51dd825a1f469a7d4da7ffb4273e29f2d3655
thehannk <108367955+thehannk@users.noreply.github.com>
2022-08-02 04:07:12 +0300

    You can use these conditions to perform different actions for different decisions.

---
+++ if-else-ternary/if-else-ternary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi günler.");
+            }
+
+            else
+            {
+                Console.WriteLine("İyi geceler.");
+            }
+
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler.";
+
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time <= 18 ? "İyi günler." : "İyi geceler."
+
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
